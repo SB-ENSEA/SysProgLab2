a17e53799672d3d5d4441cb2eccba7f5264c837b
Samuel (KML) Boccara <73236841+SB-ENSEA@users.noreply.github.com>
2022-01-16 17:53:42 +0100

    Completed gettftp.h

--- puttftp.h
+++ puttftp.h
@@ -1,1 +1,11 @@
+#define TXTSIZE 128
+#define MSGSIZE	1024
+#define PACKETSIZE 512
+
+#define TRANSMISSION_SUCCESS 1
+#define TRANSMISSION_FAILURE 0
+
 void Display(char*);
+char* WRQmsg(char*);
+int receiveAck(int,int);
+void TransmitDatagram(int, char*, int);
